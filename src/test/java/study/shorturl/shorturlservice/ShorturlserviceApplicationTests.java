516e1addd89800d6e7575cd627d6238ad305d4ea
parkkyungwon <kywonp@gmail.com>
2025-04-04 16:57:49 +0900

    new

---
+++ src/test/java/study/shorturl/shorturlservice/ShorturlserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package study.shorturl.shorturlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorturlserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
